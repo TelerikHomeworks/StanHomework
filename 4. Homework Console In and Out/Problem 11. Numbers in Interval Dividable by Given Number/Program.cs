5e0b3ab58c24139388843a1c8a9fc9f263edba9a
Stan3v <stanev@live.com>
2015-01-27 22:42:09 +0200

    in progress

---
+++ 4. Homework Console In and Out/Problem 11. Numbers in Interval Dividable by Given Number/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+/*Write a program that reads two positive integer numbers and prints how many numbers p 
+ * exist between them such that the reminder of the division by 5 is 0.*/
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
